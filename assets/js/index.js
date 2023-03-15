f8cb0becb704b953324b72eea88c7c42522d4d85
Rasel Rana <107293742+raselrana12054678@users.noreply.github.com>
2023-03-15 23:49:59 +0600

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function() {
+    $(".hide").click(function() {
+        $("p").toggle(1000);
+    })
+    
+    $(".show").click(function() {
+        $("h2").toggle(1000);
+    })
+
+    $(".Explica").click(function() {
+        $("h1").toggle(1000);
+    })
+})
